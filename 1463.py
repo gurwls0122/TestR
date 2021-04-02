0b910eb0800f1fc4c1cfee15702e6f2f3298c994
gurwls0122 <gurwls0122@naver.com>
2021-04-02 10:36:56 +0900

    1463.py

---
+++ 1463.py
@@ -0,0 +1,10 @@
+import sys
+N = int(sys.stdin.readline())
+lst = [0] * (N+1)
+for i in range(2, N+1):
+    lst[i] = lst[i-1] + 1
+    if i % 2 ==0:
+        lst[i] = min(lst[i//2]+1, lst[i])
+    if i % 3 ==0:
+        lst[i] = min(lst[i//3]+1, lst[i])
+print(lst[-1])
